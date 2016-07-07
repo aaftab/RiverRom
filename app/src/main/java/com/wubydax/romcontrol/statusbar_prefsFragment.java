a023727a374707ca37e416aa3fb6e86956b26ea3
Muhammad Aftab Amjad <aaftab@gmail.com>
2016-07-07 21:08:59 +1000

    RiverRom
    RiverRom

---
+++ app/src/main/java/com/wubydax/romcontrol/statusbar_prefsFragment.java
@@ -0,0 +1,78 @@
+package com.wubydax.romcontrol;
+/*      Created by Roberto Mariani and Anna Berkovitch, 21/06/15
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.provider.Settings;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+
+public class statusbar_prefsFragment extends PreferenceFragment {
+    HandlePreferenceFragments hpf;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        hpf = new HandlePreferenceFragments(getActivity(), this, "statusbar_prefs");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hpf.onResumeFragment();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        hpf.onPauseFragment();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        switch (requestCode) {
+            case 46:
+                String key = "notification_panel_bg";
+                Uri uri = data.getData();
+                Settings.System.putString(getActivity().getContentResolver(), key, uri.toString());
+                try {
+                    InputStream inputStream = getActivity().getContentResolver().openInputStream(uri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                    Drawable icon = new BitmapDrawable(getActivity().getResources(), bitmap);
+                    findPreference(key).setIcon(icon);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+        }
+
+    }
+
+}
